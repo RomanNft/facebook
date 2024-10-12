3360e8c8210c445a2ddaf0ad69ac71da2c6a11e1
RomanNft <118902504+RomanNft@users.noreply.github.com>
2024-10-12 16:02:33 +0300

    Create prom_stack.sh

---
+++ prometheus_stack/prom_stack.sh
@@ -0,0 +1,29 @@
+kubectl create namespace monitoring
+cd kubernetes-prometheus
+kubectl create -f clusterRole.yaml
+kubectl create -f config-map.yaml
+kubectl create  -f prometheus-deployment.yaml 
+sleep 30
+kubectl get deployments --namespace=monitoring
+kubectl create -f prometheus-service.yaml --namespace=monitoring
+
+cd ../kube-state-metrics-configs
+kubectl apply -f kube-state-metrics-configs/
+kubectl get deployments kube-state-metrics -n kube-system
+
+cd ../kubernetes-node-exporter
+kubectl create -f daemonset.yaml
+kubectl get daemonset -n monitoring
+kubectl create -f service.yaml
+kubectl get endpoints -n monitoring 
+
+cd ../kubernetes-grafana
+kubectl create -f grafana-datasource-config.yaml
+kubectl create -f deployment.yaml
+kubectl create -f service.yaml
+
+cd ../kubernetes-alert-manager
+kubectl create -f AlertManagerConfigmap.yaml
+kubectl create -f AlertTemplateConfigMap.yaml
+kubectl create -f Deployment.yaml
+kubectl create -f Service.yaml
