28a1fac436d0aa9ac29c7638b169e1b924803ab7
RomanNft <118902504+RomanNft@users.noreply.github.com>
2024-10-13 19:42:32 +0300

    Update entrypoint.sh

--- docker-elk/setup/entrypoint.sh
+++ docker-elk/setup/entrypoint.sh
@@ -6,16 +6,11 @@ set -o pipefail
 source "${BASH_SOURCE[0]%/*}"/lib.sh
 
 
-# --------------------------------------------------------
-# Users declarations
-
 declare -A users_passwords
 users_passwords=(
 	[logstash_internal]="${LOGSTASH_INTERNAL_PASSWORD:-}"
 	[kibana_system]="${KIBANA_SYSTEM_PASSWORD:-}"
-	[metricbeat_internal]="${METRICBEAT_INTERNAL_PASSWORD:-}"
 	[filebeat_internal]="${FILEBEAT_INTERNAL_PASSWORD:-}"
-	[heartbeat_internal]="${HEARTBEAT_INTERNAL_PASSWORD:-}"
 	[monitoring_internal]="${MONITORING_INTERNAL_PASSWORD:-}"
 	[beats_system]="${BEATS_SYSTEM_PASSWORD=:-}"
 )
@@ -23,25 +18,15 @@ users_passwords=(
 declare -A users_roles
 users_roles=(
 	[logstash_internal]='logstash_writer'
-	[metricbeat_internal]='metricbeat_writer'
 	[filebeat_internal]='filebeat_writer'
-	[heartbeat_internal]='heartbeat_writer'
 	[monitoring_internal]='remote_monitoring_collector'
 )
-
-# --------------------------------------------------------
-# Roles declarations
 
 declare -A roles_files
 roles_files=(
 	[logstash_writer]='logstash_writer.json'
-	[metricbeat_writer]='metricbeat_writer.json'
 	[filebeat_writer]='filebeat_writer.json'
-	[heartbeat_writer]='heartbeat_writer.json'
 )
-
-# --------------------------------------------------------
-
 
 log 'Waiting for availability of Elasticsearch. This can take several minutes.'
 
